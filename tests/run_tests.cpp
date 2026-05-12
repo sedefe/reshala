9c6025466bf12edbacf8d38f0b03ceba62f15963
nick <sagoyewatha@mail.ru>
2026-05-12 17:51:29 +0300

    tests check feasibility

--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -13,6 +13,7 @@ struct TestCase {
 
     Solution sol;
     std::chrono::microseconds time;
+    FeasibilityReport report;
 };
 
 std::vector<TestCase> ReadTestCases(const std::string& csv_path) {
@@ -57,6 +58,7 @@ bool RunTest(TestCase& tc) {
     auto read_status = io.Read(file_path.c_str());
 
     MilpModel& model = io.GetModel();
+    MilpModel model_copy = model;
     MilpSolver solver(model);
     Solution sol;
 
@@ -69,13 +71,21 @@ bool RunTest(TestCase& tc) {
     auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     tc.time = time;
 
+    if (tc.sol.status == LpStatus::kOptimal) {
+        tc.report = model_copy.GetFeasReport(tc.sol.x);
+    }
+
     return true;
 }
 
 int main() {
     std::vector<TestCase> test_cases = ReadTestCases("tests/models/results.csv");
 
+    size_t n_tests = test_cases.size();
+    size_t n_good = 0;
+
     for (auto& tc : test_cases) {
+        bool is_good = true;
         printf("%-20s: ", tc.name.c_str());
         fflush(stdout);
 
@@ -88,6 +98,7 @@ int main() {
             printf("[√] ");
         } else {
             printf("[X] (%12s)", tc.status.c_str());
+            is_good = false;
         }
 
         bool compare_y = CompareScalars(tc.y, tc.sol.y);
@@ -96,7 +107,26 @@ int main() {
             printf("[√] ");
         } else {
             printf("[X] (%.5g)", tc.y);
+            is_good = false;
         }
+
+        if (tc.sol.status == LpStatus::kOptimal) {
+            printf("Feasibility (I/B/C): ");
+            auto& [iv, bv, cv] = tc.report;
+            for (Scalar* p : {&iv, &bv, &cv}) {
+                if (iv <= kEpsZero) {
+                    printf("√");
+                } else {
+                    printf("X");
+                    is_good = false;
+                }
+            }
+        }
+
         printf("\n");
+
+        n_good += is_good;
     }
+
+    printf("%lu/%lu tests passed\n", n_good, n_tests);
 }
