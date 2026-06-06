45cffb0e0eeed74bd7ee57e2de7f8c491b56ae14
nick <sagoyewatha@mail.ru>
2026-06-06 22:39:01 +0300

    fix 7.2

--- src/presolve/rules/rule72.cpp
+++ src/presolve/rules/rule72.cpp
@@ -1,3 +1,4 @@
+#include <unordered_set>
 #include <utility>
 
 #include "reshala/presolve/rules.h"
@@ -14,49 +15,66 @@ struct Bounder {
     std::vector<Bounds> var_bounds;
     const ModelTracker& tracker;
 
-    bool Propagate(Index iv, const Bounds& new_bnd) {
+    bool Propagate(Index iv_start, const Bounds& new_bnd) {
         const MilpModel& model = tracker.GetModel();
-        const Bounds old_bnd = var_bounds[iv];
-        var_bounds[iv] = new_bnd;
 
-        std::vector<Index> changed_cons;
+        const Index kMaxIters = 5;
+        Index n_iter = 0;
 
-        for (SvIterator el(model.GetCol(iv)); el; ++el) {
-            Index ic = el.index();
-            Scalar val = el.value();
-            if (tracker.GetConMask(ic)) continue;
+        std::unordered_set<Index> changed_cons;
+        std::unordered_set<Index> changed_vars{iv_start};
 
-            Activity act = activities[ic];
-            Bounds old_range = act.GetRange();
-            act.RmTerm(val, old_bnd);
-            act.AddTerm(val, new_bnd);
-            Bounds range = act.GetRange();
-            if (StrongGt(range.le, range.ri)) {
-                return false;
-            }
+        std::vector<Bounds> old_bounds = var_bounds;
+        var_bounds[iv_start] = new_bnd;
 
-            if (range.le != old_range.le or range.ri != old_range.ri) {  // todo weak comparison
-                activities[ic] = act;
-                changed_cons.push_back(ic);
-            }
-        }
+        while (!changed_vars.empty() and n_iter < kMaxIters) {
+            n_iter++;
 
-        for (auto ic : changed_cons) {
-            for (SvIterator el(model.GetRow(ic)); el; ++el) {
-                Index iv1 = el.index();
-                if (tracker.GetVarMask(iv1)) continue;
+            changed_cons.clear();
+            for (auto iv : changed_vars) {  // var_bounds -> activities
+                for (SvIterator el(model.GetCol(iv)); el; ++el) {
+                    Index ic = el.index();
+                    Scalar val = el.value();
+                    if (tracker.GetConMask(ic)) continue;
 
-                Scalar val = el.value();
-                const Bounds& bnd = var_bounds[iv1];
-                Bounds derived = tracker.DeriveBounds(ic, iv1, activities[ic], bnd, val);
-
-                if (StrongGt(derived.le, bnd.le) or StrongLt(derived.ri, bnd.ri)) {
-                    Bounds new_bnd = {std::max(bnd.le, derived.le), std::min(bnd.ri, derived.ri)};
-
-                    if (new_bnd.le > new_bnd.ri) {
+                    Activity act = activities[ic];
+                    Bounds old_range = act.GetRange();
+                    act.RmTerm(val, old_bounds[iv]);
+                    act.AddTerm(val, var_bounds[iv]);
+                    Bounds new_range = act.GetRange();
+                    if (StrongGt(new_range.le, new_range.ri)) {
                         return false;
                     }
-                    var_bounds[iv1] = new_bnd;
+
+                    if (new_range.le != old_range.le or
+                        new_range.ri != old_range.ri) {  // todo weak comparison
+                        activities[ic] = act;
+                        changed_cons.emplace(ic);
+                    }
+                }
+            }
+
+            old_bounds = var_bounds;
+
+            changed_vars.clear();
+            for (auto ic : changed_cons) {  // activities -> var_bounds
+                for (SvIterator el(model.GetRow(ic)); el; ++el) {
+                    Index iv = el.index();
+                    if (tracker.GetVarMask(iv)) continue;
+
+                    Scalar val = el.value();
+                    const Bounds& old_bnd = old_bounds[iv];
+                    Bounds& curr_bnd = var_bounds[iv];
+                    Bounds derived = tracker.DeriveBounds(ic, iv, activities[ic], old_bnd, val);
+
+                    if (StrongGt(derived.le, curr_bnd.le) or StrongLt(derived.ri, curr_bnd.ri)) {
+                        curr_bnd = {std::max(curr_bnd.le, derived.le),
+                                    std::min(curr_bnd.ri, derived.ri)};
+                        if (curr_bnd.le > curr_bnd.ri + kEpsZero) {
+                            return false;
+                        }
+                        changed_vars.emplace(iv);
+                    }
                 }
             }
         }
@@ -84,14 +102,14 @@ RuleResult Rule72::Apply(ModelTracker& tracker) {
             return RuleResult::kInfeasible;
         }
         if (!results[0]) {
-            // printf("x%d -> 0\n", iv);
-            tracker.UpdVarBounds(iv, {Scalar(0), Scalar(0)});
+            // printf("x%d -> 1\n", iv);
+            tracker.UpdVarBounds(iv, {Scalar(1), Scalar(1)});
             n_reduced++;
             continue;
         }
         if (!results[1]) {
-            // printf("x%d -> 1\n", iv);
-            tracker.UpdVarBounds(iv, {Scalar(1), Scalar(1)});
+            // printf("x%d -> 0\n", iv);
+            tracker.UpdVarBounds(iv, {Scalar(0), Scalar(0)});
             n_reduced++;
             continue;
         }
@@ -108,7 +126,7 @@ RuleResult Rule72::Apply(ModelTracker& tracker) {
                 if (StrongGt(new_bnd.le, new_bnd.ri)) {
                     return RuleResult::kInfeasible;
                 }
-                tracker.UpdVarBounds(iv1, derived);
+                tracker.UpdVarBounds(iv1, new_bnd);
                 n_reduced++;
             }
         }
